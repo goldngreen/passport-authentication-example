4417bdd404226931306746e91325fa000b1916b8
afs <afs@goldngreen.co.uk>
2021-03-20 10:41:48 +0000

    Remove logging noise from test-db

--- test/test-db.js
+++ test/test-db.js
@@ -19,32 +19,23 @@ describe('db', () => {
     });
 
     it('should support create', () => {
-        console.log("Total users: " + db.users.count());
         let records = [];
         for (i = 0; i < 2; i++) {
             records.push( { username: "rec" + i, password: "recpass" + i, displayName: "Record " + i, email: "rec" + i + "@example.com" } );
         }
-        console.log("Total users: " + db.users.count());
 
         records.forEach(r => testCreateFindByUser(r));
-        console.log("Done testCreateFindByUser, total users: " + db.users.count());
-
         records.forEach(r => db.users.remove(r.id));
-        console.log("Done deletes, total users: " + db.users.count());
     });
 
     it('should support fetch', () => {
-        console.log("Empty test");
         // let records = db.users.fetch();
         // assert.notStrictEqual(db.users.fetch(), null)
     });
-
-    const dummy = { id: -1, provider: '' };
 });
 
    
 testCreateFindByUser = (record) => {
-    console.log("Do test" + record);
     db.users.create(record);
 
     db.users.findByUsername(record.username, (err, user) => {
