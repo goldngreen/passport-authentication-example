9e7e4e1b40ff539751e1200942b3482fe57d44b5
Andy Sinclair <goldngreen@users.noreply.github.com>
2021-04-19 17:59:37 +0100

    Very basic ajax authentication

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,13 @@
 
 		
-function ajax(form, requestParams) {
+function ajax(url, requestParams) {
     requestParams.timeout = 120000;
     $.ajax({
         method: 'post',
-        url: form.action,
+        url: url,
         complete: function(result, request) {
             console.log(result);
+            window.location.replace("/");
         },
         error: function(result, request) {
             console.log('Error calling ' + this.data.method);
